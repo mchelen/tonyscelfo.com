d4498627eb92720b7987702c7a64b61864a23ad6
Tony Scelfo <scelfo@tonyscelfo.com>
2013-07-30 22:17:41 -0700

    add bachelor party redirects

--- _app/redirect.py
+++ _app/redirect.py
@@ -5,6 +5,7 @@ redirects = {
     '/<:\+|plus>': 'https://plus.google.com/110371866564374610674',
     '/smint': 'http://stats.pingdom.com/uoumvhl0hdy0/495263',
     '/wordpress/2008/03/01/hacking-appletv-take-2<:/?>': '/2008/09/15/hacking-appletv.html',
+    '/<:bachelorparty|bourbon|kentucky>': 'https://docs.google.com/document/d/1uscJ7-y6XnuZwGWzSmZk70RQS1CM3v8jDwKw405-jX8/edit',
 }
 
 wsgi = webapp2.WSGIApplication(
